5207eec9e82ce59f4e43ef570e8f197b3fa81bd9
Ramozjikevic <bro.brobro@mail.ru>
2020-10-24 20:58:57 +0500

    rename class

--- app/src/main/java/ru/ramozjikevic/socialmap/ui/RootActivity.kt
+++ app/src/main/java/ru/ramozjikevic/socialmap/ui/RootActivity.kt
@@ -1,11 +1,12 @@
-package ru.ramozjikevic.socialmap
+package ru.ramozjikevic.socialmap.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.ramozjikevic.socialmap.R
 
-class MainActivity : AppCompatActivity() {
+class RootActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_root)
     }
 }
