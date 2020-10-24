9d5159d9a2c1bd7918dfa521d096b28a30736405
Ramozjikevic <bro.brobro@mail.ru>
2020-10-25 02:06:04 +0500

    add base activity

---
+++ app/src/main/java/ru/ramozjikevic/socialmap/ui/component/base/BaseActivity.kt
@@ -0,0 +1,33 @@
+package ru.ramozjikevic.socialmap.ui.component.base
+
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import dagger.android.AndroidInjection
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasAndroidInjector
+import ru.ramozjikevic.socialmap.ui.component.base.viewmodel.BaseRxViewModel
+import ru.ramozjikevic.socialmap.ui.component.base.viewmodel.IViewModelState
+import javax.inject.Inject
+
+abstract class BaseActivity<T : BaseRxViewModel<out IViewModelState>> : AppCompatActivity(), HasAndroidInjector {
+
+    protected abstract val layout: Int
+    protected abstract val viewModel: T
+
+    @Inject
+    lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Any>
+
+    @Inject
+    lateinit var viewModelFactory: ViewModelProvider.Factory
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        AndroidInjection.inject(this)
+        super.onCreate(savedInstanceState)
+        setContentView(layout)
+    }
+}
+
+inline fun <reified T : BaseRxViewModel<out IViewModelState>> BaseActivity<T>.viewModels(
+) = viewModels<T> { viewModelFactory }
